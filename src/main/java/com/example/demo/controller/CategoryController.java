6a995680bdb95b40ffa291ac9fd897d174e0c411
오병권 <ohbg555@gmail.com>
2022-02-02 15:58:02 +0900

    .

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.lang.reflect.InvocationTargetException;
 
@@ -24,11 +25,23 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    public ResponseEntity<ResponseComDto> createCategory(CategoryDto.createSingle categoryDto) throws InvocationTargetException,
+    @PostMapping("/create")
+    public ResponseEntity<ResponseComDto> createSingleCategory(CategoryDto.createSingle categoryDto) throws InvocationTargetException,
             NoSuchMethodException, InstantiationException, IllegalAccessException {
         categoryService.createSingleCategory(categoryDto);
         return new ResponseEntity<>(
                 ResponseComDto.builder()
+                        .resultMsg("카테고리 생성 완료")
+                        .resultObj(null)
+                        .build(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create/all")
+    public ResponseEntity<ResponseComDto> createAllCategory(CategoryDto.create categoryDto) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException {
+        categoryService.createAllCategory(categoryDto);
+        return new ResponseEntity<>(
+                com.example.demo.dto.ResponseComDto.builder()
                         .resultMsg("카테고리 생성 완료")
                         .resultObj(null)
                         .build(), HttpStatus.OK);
